ce9ece290ff3e4eb4d15da7914b5e774a6bd83b6
hezonglianheng <976410976@qq.com>
2024-05-12 17:34:59 +0800

    标注语料空间角色并与依存语法标注结果合并

--- task2/config.py
+++ task2/config.py
@@ -2,11 +2,11 @@
 # config for scripts in the task2.
 
 # 文件路径
-ORIGIN_DATA_PATH = r'spatial_synonym_data.xlsx'
-DEP_RESULT_PATH = r'dep_result.json'
-ANNOTATE_NEED_PATH = r'annotate_need.jsonl'
-ANNOTATE_RES_PATH = r'annotate_result.jsonl'
-FINAL_RES_PATH = r'final_result.json'
+ORIGIN_DATA_PATH = r'spatial_synonym_data.xlsx' # 原始数据
+DEP_RESULT_PATH = r'dep_result.json' # 依存句法分析结果
+ANNOTATE_NEED_PATH = r'annotate_need.jsonl' # 需要做空间信息标注的数据
+ANNOTATE_RES_PATH = r'annotation_result.jsonl' # 空间信息标注结果
+DEP_ANN_RES_PATH = r'dep_ann_result.json' # 合并依存句法分析结果和空间信息标注结果
 
 ZH_MODEL = 'zh_core_web_md' # 中文模型名称
 
@@ -31,11 +31,17 @@ SPATIAL = 'spatial' # 空间信息
 TRAJECTORY = 'trajectory' # 射体
 LANDMARK = 'landmark' # 地标
 EVENT = 'event' # 事件
+CONTAIN_SEG = 'contain_seg' # 包含的分词结果项目
 
 # jsonlines文件字段名称
 PAIR = 'pair' # 句对中的编号
 TEXT = 'text' # 句子
 LABEL = 'label' # 标签
+
+# 标注结果中的标签
+ENTITIES = 'entities' # 实体
+START_OFFSET = 'start_offset' # 开始位置
+END_OFFSET = 'end_offset' # 结束位置
 
 # 依存句法分析结果中的关系
 # 特殊关系ROOT
